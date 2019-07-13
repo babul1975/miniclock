c934485ff3b3dc72d084fe6d0daeac3894ec6a63
Ratti3 <52182178+Ratti3@users.noreply.github.com>
2019-07-13 23:34:00 +0100

    Add files via upload
    Added hyphen needed by UTC offset menu

--- ProgmemData.h
+++ ProgmemData.h
@@ -58,3 +58,61 @@ const char t_3[] PROGMEM = "forty";
 const char t_4[] PROGMEM = "fifty";
 
 const char *const numberstens[] PROGMEM = {t_0, t_1, t_2, t_3, t_4};
+
+// Main menu
+const char mm_0[] PROGMEM = ">Basic";
+const char mm_1[] PROGMEM = ">Small";
+const char mm_2[] PROGMEM = ">Slide";
+const char mm_3[] PROGMEM = ">Words";
+const char mm_4[] PROGMEM = ">Setup";
+
+const char *const mainmenu[] PROGMEM = {mm_0, mm_1, mm_2, mm_3, mm_4};
+
+// Clock Set menu
+const char c_0[] PROGMEM = ">Set Min";
+const char c_1[] PROGMEM = ">Set Hr";
+const char c_2[] PROGMEM = ">Set Day";
+const char c_3[] PROGMEM = ">Set Mth";
+const char c_4[] PROGMEM = ">Set Yr";
+
+const char *const clockset[] PROGMEM = {c_0, c_1, c_2, c_3, c_4};
+
+// Setup menu
+const char s_0[] PROGMEM = ">Rnd Clk";
+const char s_1[] PROGMEM = ">Rnd Fnt";
+const char s_2[] PROGMEM = ">12 Hr";
+const char s_3[] PROGMEM = ">Font";
+const char s_4[] PROGMEM = ">NTP DST";
+const char s_5[] PROGMEM = ">D/Time";
+const char s_6[] PROGMEM = ">Auto LX";
+const char s_7[] PROGMEM = ">Bright";
+const char s_8[] PROGMEM = ">Exit";
+
+const char *const setupmenu[] PROGMEM = {s_0, s_1, s_2, s_3, s_4, s_5, s_6, s_7, s_8};
+
+// Bool menu
+const char b_0[] PROGMEM = ">Set DST";
+const char b_1[] PROGMEM = ">Set NTP";
+const char b_2[] PROGMEM = ">Set 12h";
+const char b_3[] PROGMEM = ">Set Rnd";
+const char b_4[] PROGMEM = ">Set LX";
+
+const char *const boolmenu[] PROGMEM = {b_0, b_1, b_2, b_3, b_4};
+
+// Display Options
+const char o_0[] PROGMEM = ">Normal";
+const char o_1[] PROGMEM = ">On";
+const char o_2[] PROGMEM = ">Off";
+const char o_3[] PROGMEM = "> 9.00pm";
+const char o_4[] PROGMEM = ">10.00pm";
+const char o_5[] PROGMEM = ">11.00pm";
+
+const char *const displayoptions[] PROGMEM = {o_0, o_1, o_2, o_3, o_4, o_5};
+
+// Date suffix
+const char ds_0[] PROGMEM = "st";
+const char ds_1[] PROGMEM = "nd";
+const char ds_2[] PROGMEM = "rd";
+const char ds_3[] PROGMEM = "th";
+
+const char *const suffix[] PROGMEM = {ds_0, ds_1, ds_2, ds_3};
