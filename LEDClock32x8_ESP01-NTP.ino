2c5e9cfef1a50d4060db297bf76e90840f00cafc
Ratti3 <52182178+Ratti3@users.noreply.github.com>
2019-07-16 20:59:54 +0100

    Add files via upload
    Various issues fixed

--- LEDClock32x8_ESP01-NTP.ino
+++ LEDClock32x8_ESP01-NTP.ino
@@ -16,7 +16,7 @@ Tested on IDE v1.8.9
 1,265 bytes 61%
 
 https://github.com/Ratti3/miniclock
-https://youtu.be/CpQsMjI3FL0
+https://youtu.be/krdAU_GUc3k
 https://create.arduino.cc/projecthub/Ratti3/led-matrix-word-clock-with-bme280-bh1750-and-esp01-fdde2b
 
 ***********************************************************************/
