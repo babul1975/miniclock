422c765edef8f8822d10d3b145475e5dec54c601
Ratti3 <52182178+Ratti3@users.noreply.github.com>
2019-07-16 20:59:14 +0100

    Add files via upload
    SSID and Password moved to Arduino, more error handling

--- ESP-01_NTP.ino
+++ ESP-01_NTP.ino
@@ -1,14 +1,14 @@
 /***********************************************************************
 
-Mini Clock ESP01 code by Ratti3 - 14 Jul 2019
+Mini Clock ESP01 code by Ratti3 - 16 Jul 2019
 Distributed under the terms of the GPL.
 Tested on IDE v1.8.9
 
-268,480 bytes 53%
-27,112 bytes 33%
+268,688 bytes 53%
+27,232 bytes 33%
 
 https://github.com/Ratti3/miniclock
-https://youtu.be/CpQsMjI3FL0
+https://youtu.be/krdAU_GUc3k
 https://create.arduino.cc/projecthub/Ratti3/led-matrix-word-clock-with-bme280-bh1750-and-esp01-fdde2b
 
 ***********************************************************************/
@@ -23,8 +23,9 @@ extern "C" {
 
 #define FPM_SLEEP_MAX_TIME 0xFFFFFFF
 
-char ssid[] = "SSID";        // your network SSID (name)
-char pass[] = "PASSWORD";         // your network password
+// Variable for SSID and Password
+char ssid[40];
+char pass[40];
 
 const char* ntpServerName = "pool.ntp.org"; // the NTP pool to query
 byte try_count = 15;                        // number of packet send attempts, 1 try_count = 2 seconds
@@ -39,14 +40,16 @@ byte count;          // counter for retrying packets
 byte retry_count;    // holds the total attempts so far
 byte retry_max;      // number of total attempts (retry_count * try_count), this value is obtained from the Arduino
 char buf[1];         // hold the retry_max from the Arduino, so it can be converted
-char buffer[20];     // holds the data received on software serial
+char buffer[80];     // holds the data received on software serial
+bool WiFiFail = 0;   // Holds status for WiFi connection failure
 
 WiFiUDP udp;         // WiFi UDP library
 
 
 void setup() {
 
-  Serial.begin(9600);  // max speed as using softwareserial on Arduino
+  // max speed as using softwareserial on Arduino
+  Serial.begin(9600);
 
   // Begin UDP library
   udp.begin(localPort);
@@ -58,12 +61,57 @@ void loop() {
 
   // Read the software serial and look for NTP string received from the Arduino
   // The number after
-  if (readline(Serial.read(), buffer, 20) > 0) {
-    if ((buffer[0] == 'N') && (buffer[1] == 'T') && (buffer[2] == 'P')) {
+  if (readline(Serial.read(), buffer, 80) > 0) {
+    if (buffer[0] == 'N' && buffer[1] == 'T' && buffer[2] == 'P') {
+
+      // Work out the length of the SSID
+      char ssid_len[2];
+      ssid_len[0] = buffer[4];
+      ssid_len[1] = buffer[5];
+      ssid_len[2] = '\0';
+      int ssid_length = atoi(ssid_len);
+
+      // Using the calculated length, work out the SSID name
+      byte i = 6;
+      byte x = 0;
+      while (i < ssid_length + 6) {
+        ssid[x] = buffer[i];
+        i++;
+        x++;
+      }
+      ssid[ssid_length] = '\0';
+
+      // Work out the length of the SSID password
+      char pass_len[2];
+      pass_len[0] = buffer[ssid_length + 6];
+      pass_len[1] = buffer[ssid_length + 7];
+      pass_len[2] = '\0';
+      int pass_length = atoi(pass_len);
+
+      // Using the calculated length, work out the SSID password
+      i = ssid_length + 8;
+      x = 0;
+      while (i < pass_length + ssid_length + 8) {
+        pass[x] = buffer[i];
+        i++;
+        x++;
+      }
+      pass[pass_length] = '\0';
+
+      //Uncomment to troubleshoot SSID and Password
+      //Serial.println(ssid_len);
+      //Serial.println(ssid);
+      //Serial.println(pass_len);
+      //Serial.println(pass);
+
       retry_count = 1;
+      // Work out the retry count received from the Arduino
       buf[0] = buffer[3];
+      buf[1] = '\0';
       retry_max = atoi(buf);
-      Serial.println("ESP01 Says: NTP request received from Arduino");
+
+      // Send an acknowledgement to the Arduino
+      Serial.println("ESP01 : NTP request received from Arduino");
       while (retry_count <= retry_max) {
         Serial.print("[attempt ");
         Serial.print(retry_count);
@@ -71,12 +119,17 @@ void loop() {
         Serial.print(retry_max);
         Serial.println("]");
         WiFiOn();
+        if (WiFiFail) {
+          break;
+        }
         GetNTPTime();
         WiFiOff();
         retry_count++;
         if (cb) break;
         if (retry_count > retry_max) {
+          Serial.println(" ");
           Serial.println("NTP Fail");
+          Serial.println(" ");
         }
       }
     }
@@ -85,6 +138,8 @@ void loop() {
 
 }
 
+
+// Main routine for getting NTP time and sending to Arduino via Serial
 void GetNTPTime() {
   
   WiFi.hostByName(ntpServerName, timeServerIP);
@@ -116,12 +171,15 @@ void GetNTPTime() {
     unsigned long secsSince1900 = highWord << 16 | lowWord;
     const unsigned long seventyYears = 2208988800UL;
     unsigned long epoch = secsSince1900 - seventyYears;
+    // The end result is here, the UNIX time, send it to the Arduino via Serial
     Serial.print("UNIX");
     Serial.println(epoch);
   }
   
 }
 
+
+// Turns on WiFi
 void WiFiOn() {
 
   wifi_fpm_do_wakeup();
@@ -132,6 +190,7 @@ void WiFiOn() {
 
 }
 
+// Turns off WiFi
 void WiFiOff() {
 
   Serial.print("Disabling WiFi..");
@@ -147,28 +206,38 @@ void WiFiOff() {
 
 }
 
+
+// Connects WiFi
 void wifi_connect() {
 
   count = 1;
-  Serial.println();
-  // attempt to connect to Wifi network:
+  WiFiFail = 0;
   WiFi.begin(ssid, pass);
   Serial.print("Connecting to SSID: ");
   Serial.print(ssid);
   Serial.print("..");
   while (WiFi.status() != WL_CONNECTED) {
     WiFi.mode(WIFI_STA);
-    delay(500);
+    delay(1000);
     count++;
-    if (count > 15) {
+    if (count > 7) {
+      Serial.println(" ");
       Serial.println("WiFi Fail");
+      Serial.println(" ");
+      WiFiFail = 1;
+      WiFiOff();
+      break;
     }
   }
-  Serial.println("..connected");
-  printWifiStatus();
+  if (!WiFiFail) {
+    Serial.println("..connected");
+    printWifiStatus();
+  }
 
 }
 
+
+// Prints WiFi connection info
 void printWifiStatus() {
 
   // print the SSID of the network you're attached to:
@@ -188,6 +257,8 @@ void printWifiStatus() {
 
 }
 
+
+// Build UDP packet for sending
 unsigned long sendNTPpacket(IPAddress& address) {
   
   Serial.print("Sending NTP packet to: ");
@@ -212,7 +283,8 @@ unsigned long sendNTPpacket(IPAddress& address) {
 
 }
 
-//used to readline from serial output
+
+// Used to readline from serial output
 int readline(int readch, char *buffer, int len) {
 
   static int pos = 0;
